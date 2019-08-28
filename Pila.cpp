071cd1b2361427632dea70d2514899952d047438
Alfredo Villagran Olguin <avillagran@ipn.mx>
2019-08-28 07:44:25 -0500

    Clase Pila

---
+++ Pila.cpp
@@ -0,0 +1,36 @@
+#include "Pila.h"
+
+Pila::Pila(void){
+    top = NULL;
+}
+void Pila::push(int d){
+    top = new Nodo(d,top);
+}
+int Pila::pop(void){
+    int d;
+    Nodo* aux;
+    d = top->dameDato();
+    aux = top->dameSig();
+    delete top;
+    top = aux;
+    return d;
+}
+void Pila::push(void){
+    int d;
+    cout<<"Dame d ";cin>>d;
+    top = new Nodo(d,top);
+}
+bool Pila::estaVacia(void){
+    if(top == NULL)
+        return true;
+    else
+
+        return false;
+}
+void Pila::vaciaPila(void){
+    while(!estaVacia())
+        cout << pop() << endl;
+}
+Pila::~Pila(){
+    vaciaPila();
+}
---
+++ Pila.h
@@ -0,0 +1,22 @@
+#ifndef PILA_H
+#define PILA_H
+
+#include <iostream>
+#include "Nodo.h"
+using namespace std;
+
+class Pila{
+private:
+    Nodo* top;
+public:
+    Pila(void);
+    ~Pila(void);
+    void push(int d);
+    int pop(void);
+    void push(void);
+    bool estaVacia(void);
+    void vaciaPila(void);
+};
+
+#endif // PILA_H
+
