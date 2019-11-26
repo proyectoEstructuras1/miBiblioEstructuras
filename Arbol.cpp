7f2962c3d064a3d497bc6fca5c40d01f70e79f50
proyectoEstructuras1 <57818462+proyectoEstructuras1@users.noreply.github.com>
2019-11-25 19:21:08 -0600

    Update Arbol.cpp

--- Arbol.cpp
+++ Arbol.cpp
@@ -1,125 +1,181 @@
-#include "Arbol.h"
+#include "ArbolP.h"
+
+ArbolP::ArbolP()
+{
+    raiz=NULL;
+}
+
+ArbolP::~ArbolP()
+{
+  eliminaArbol();
+}
+//rid
+void ArbolP::muestraPre(void)
+{
+ muestraPre(raiz);
+}
+void ArbolP::muestraPre(NodoA* r)
+{
+if(r!=NULL)
+{
+    r->muestraDato();
+    muestraPre(r->dameIzq());
+    muestraPre(r->dameDer());
 
 
-Arbol::Arbol(void){
-    raiz = NULL;
 }
-Arbol::~Arbol(void){
-    eliminaArbol();
+
 }
-void Arbol::muestraPre(void){
-    muestraPre(raiz);
+//idr
+void ArbolP::muestraPos(void)
+{
+muestraPos(raiz);
+
 }
-void Arbol::muestraPre(NodoA* r){
-    if(r!=NULL){
-        r->muestraDato();
-        muestraPre(r->dameIzq());
-        muestraPre(r->dameDer());
+void ArbolP::muestraPos(NodoA* r)
+{
+if(r!=NULL)
+{
+    muestraPos(r->dameIzq());
+    muestraPos(r->dameDer());
+    r->muestraDato();
+
+}
+
+
+}
+//ird
+void ArbolP::muestraIn (void)
+{
+muestraIn(raiz);
+
+}
+void ArbolP::muestraIn (NodoA* r)
+{
+    if(r!=NULL)
+    {
+     muestraIn(r->dameIzq());
+     r->muestraDato();
+     muestraIn(r->dameDer());
+
+    }
+
+}
+void ArbolP::insertaNodo(Producto d)
+{
+if(raiz==NULL)
+{
+ raiz=new NodoA(d,NULL,NULL);
+
+
+}
+else
+insertaNodo(raiz,d);
+
+}
+void ArbolP::insertaNodo(NodoA* r,Producto d)
+{
+ if (r->dameIzq()==NULL && d.dameNumero()<=r->dameDato().dameNumero())
+
+    r->modificaIzq(new NodoA(d,NULL,NULL));
+else if (r->dameDer()==NULL && d.dameNumero()>r->dameDato().dameNumero())
+    r->modificaDer(new NodoA(d,NULL,NULL));
+else if (r->dameIzq()!=NULL&& d.dameNumero()<=r->dameDato().dameNumero())
+    insertaNodo(r->dameIzq(),d);
+else if(r->dameDer()!=NULL && d.dameNumero()>r->dameDato().dameNumero())
+    insertaNodo(r->dameDer(),d);
+
+}
+void ArbolP::eliminaArbol(void)
+{
+ if(raiz!=NULL)
+ {
+     eliminaArbol(raiz);
+     raiz=NULL;
+
+ }
+
+}
+void ArbolP::eliminaArbol(NodoA* r)
+{
+
+
+if(r!=NULL)
+    {eliminaArbol(r->dameIzq());
+    eliminaArbol(r->dameDer());
+
+    r->dameDato().muestraDatos();
+
+    delete r;
     }
 }
-void Arbol::muestraIn(void){
-    muestraIn(raiz);
-}
-void Arbol::muestraIn(NodoA* r){
-    if(r!=NULL){
-        muestraIn(r->dameIzq());
-        r->muestraDato();
-        muestraIn(r->dameDer());
-    }
-}
-void Arbol::muestraPos(void){
-    muestraPos(raiz);
-}
-void Arbol::muestraPos(NodoA* r){
-    if(r!=NULL){
-        muestraPos(r->dameIzq());
-        muestraPos(r->dameDer());
-        r->muestraDato();
-    }
-}
-void Arbol::insertaNodo(int d){
+bool ArbolP::buscaNodo(int d)
+{
     if(raiz==NULL)
-        raiz = new NodoA(NULL,d,NULL);
-    else
-        insertaNodo(raiz,d);
-}
-void Arbol::insertaNodo(NodoA* r, int d){
-    if(d<=r->dameDato() && r->dameIzq()==NULL)
-        r->modificaIzq(new NodoA(NULL,d,NULL));
-    else if(d>r->dameDato() && r->dameDer()==NULL)
-        r->modificaDer(new NodoA(NULL,d,NULL));
-    else if(d<=r->dameDato() && r->dameIzq()!=NULL)
-        insertaNodo(r->dameIzq(),d);
-    else if(d>r->dameDato() && r->dameDer()!=NULL)
-        insertaNodo(r->dameDer(),d);
-}
-void Arbol::eliminaArbol(void){
-    if(raiz!=NULL){
-        eliminaArbol(raiz);
-        raiz=NULL;
-    }
-}
-void Arbol::eliminaArbol(NodoA* r){
-    if(r!=NULL){
-        eliminaArbol(r->dameIzq());
-        eliminaArbol(r->dameDer());
-        r->muestraDato();
-        delete r;
-    }
-}
-bool Arbol::buscaNodo(int d){
-    if(raiz==NULL)
-        return false;
-    else if(d==raiz->dameDato())
+    return false;
+    else if (d==raiz->dameDato().dameNumero())
         return true;
     else
-        return buscaNodo(raiz,d);
+    return buscaNodo(raiz,d);
+
+
 }
-bool Arbol::buscaNodo(NodoA* r, int d){
-    if(d==r->dameDato())
-        return true;
-    else if(d<r->dameDato() && r->dameIzq()==NULL)
+bool ArbolP::buscaNodo(NodoA* r, int d)
+{
+
+if(d==r->dameDato().dameNumero())
+{
+ r->dameDato().muestraDatos();
+        return true;}
+    else if(d<r->dameDato().dameNumero() && r->dameIzq()==NULL)
         return false;
-    else if(d>r->dameDato() && r->dameDer()==NULL)
+    else if(d>r->dameDato().dameNumero() && r->dameDer()==NULL)
         return false;
-    else if(d<r->dameDato())
+    else if(d<r->dameDato().dameNumero())
         return buscaNodo(r->dameIzq(),d);
-    else if(d>r->dameDato())
+    else if(d>r->dameDato().dameNumero())
         return buscaNodo(r->dameDer(),d);
+
+
+
 }
-int Arbol::cuentaNodos(void){
-    if(raiz==NULL)
-        return 0;
-    else
-        return cuentaNodos(raiz);
+int ArbolP::cuentaNodos(void)
+{
+if(raiz==NULL)
+    return 0;
+else
+    return cuentaNodos(raiz);
+
+
 }
-int Arbol::cuentaNodos(NodoA* r){
-    if(r->dameIzq()==NULL &&
-       r->dameDer()==NULL){
-        return 1;
-    }
-    else if(r->dameIzq()!=NULL &&
-       r->dameDer()==NULL){
-        return 1 + cuentaNodos(r->dameIzq());
-    }
-    else if(r->dameIzq()==NULL &&
-       r->dameDer()!=NULL){
-        return 1 + cuentaNodos(r->dameDer());
-    }
-    else {
-        return 1 + cuentaNodos(r->dameIzq())
-                 + cuentaNodos(r->dameDer());
-    }
+int ArbolP::cuentaNodos(NodoA* r)
+{
+if(r->dameIzq()==NULL && r->dameDer()==NULL)
+return 1;
+
+else if(r->dameIzq()!=NULL && r->dameDer()==NULL)
+   return  1+ cuentaNodos(r->dameIzq());
+else if (r->dameDer()!=NULL && r->dameIzq()==NULL)
+    return 1+cuentaNodos(r->dameDer());
+else
+    return 1+cuentaNodos(r->dameIzq())+cuentaNodos(r->dameDer());
+
+
 }
-int Arbol::profundidadDelArbol(void){
-    int pP = -1;
+int ArbolP::profundidadDelArbol(void)
+{
+int pP = -1;
     if(raiz==NULL)
         return pP;
     else
         return profundidadDelArbol(raiz,pP);
+
+
+
 }
-int Arbol::profundidadDelArbol(NodoA* r, int pP){
-    if(r->dameIzq()==NULL && r->dameDer()==NULL)
+int ArbolP::profundidadDelArbol(NodoA* r, int pP)
+{
+ if(r->dameIzq()==NULL && r->dameDer()==NULL)
         return pP + 1;
     else if(r->dameIzq()!=NULL && r->dameDer()==NULL)
         return profundidadDelArbol(r->dameIzq(),pP + 1);
@@ -134,99 +190,107 @@ int Arbol::profundidadDelArbol(NodoA* r, int pP){
         else
             return pD;
     }
+
+
 }
-bool Arbol::eliminaSubarbol(int d){
+bool ArbolP::eliminaSubarbol(int d)
+{
     if(raiz==NULL)
         return false;
-    else if(d==raiz->dameDato()){
+    else if(d==raiz->dameDato().dameNumero()){
         eliminaArbol();
         return true;
     }
     else
         return eliminaSubarbol(NULL,raiz,d);
 }
-bool Arbol::eliminaSubarbol(NodoA* nP,NodoA* r,int d){
-    if(d==r->dameDato()&&r==nP->dameIzq()){
+bool ArbolP::eliminaSubarbol(NodoA* nP,NodoA* r,int d){
+    if(d==r->dameDato().dameNumero()&&r==nP->dameIzq()){
         eliminaArbol(r);
         nP->modificaIzq(NULL);
         return true;
     }
-    else if(d==r->dameDato()&&r==nP->dameDer()){
+    else if(d==r->dameDato().dameNumero()&&r==nP->dameDer()){
         eliminaArbol(r);
         nP->modificaDer(NULL);
         return true;
     }
-    else if(d<r->dameDato()&&r->dameIzq()==NULL)
+    else if(d<r->dameDato().dameNumero()&&r->dameIzq()==NULL)
         return false;
-    else if(d>r->dameDato()&&r->dameDer()==NULL)
+    else if(d>r->dameDato().dameNumero()&&r->dameDer()==NULL)
         return false;
-    else if(d<r->dameDato()&&r->dameIzq()!=NULL)
+    else if(d<r->dameDato().dameNumero()&&r->dameIzq()!=NULL)
         return eliminaSubarbol(r,r->dameIzq(),d);
-    else if(d>r->dameDato()&&r->dameDer()!=NULL)
+    else if(d>r->dameDato().dameNumero()&&r->dameDer()!=NULL)
         return eliminaSubarbol(r,r->dameDer(),d);
 }
-bool Arbol::eliminaNodo(int d){
-    NodoA* aux;
-    if(raiz==NULL){
-        cout<<"Arbol vacio"<<endl<<endl;
+
+bool ArbolP::eliminaNodo(int d)
+{
+   if(raiz==NULL)
         return false;
+    else if(d==raiz->dameDato().dameNumero()){
+        eliminaArbol();
+        return true;
     }
     else
         return eliminaNodo(NULL,raiz,d);
+
+
+
 }
-bool Arbol::eliminaNodo(NodoA* nP,NodoA* r,int d){
-    if(d==r->dameDato() && r->dameIzq()==NULL && r->dameDer()==NULL){//Eres hoja
+bool ArbolP::eliminaNodo(NodoA* nP,NodoA* r,int d){
+    if(d==r->dameDato().dameNumero() && r->dameIzq()==NULL && r->dameDer()==NULL){
         delete r;
 
         if(nP==NULL)//Y eres la raiz
             raiz=NULL;
-        else if(r==nP->dameIzq())//Y eres hijo izquierdo de nP
+        else if(r==nP->dameIzq())
             nP->modificaIzq(NULL);
         else//Y eres hijo derecho de nP
             nP->modificaDer(NULL);
 
         return true;
     }
-    else if(d==r->dameDato() && r->dameIzq()!=NULL && r->dameDer()==NULL){//Solo tienes hijo izquierdo
-
-        if(nP==NULL)//Y eres la raiz
+    else if(d==r->dameDato().dameNumero() && r->dameIzq()!=NULL && r->dameDer()==NULL){
+        if(nP==NULL)
             raiz=r->dameIzq();
-        else if(r==nP->dameIzq())//Y eres hijo izquierdo de nP
+        else if(r==nP->dameIzq())
             nP->modificaIzq(r->dameIzq());
-        else//Y eres hijo derecho de nP
+        else
             nP->modificaDer(r->dameIzq());
 
         delete r;
         return true;
     }
-    else if(d==r->dameDato() && r->dameIzq()==NULL && r->dameDer()!=NULL){//Solo tienes hijo derecho
-
-        if(nP==NULL)//Y eres la raiz
+    else if(d==r->dameDato().dameNumero() && r->dameIzq()==NULL && r->dameDer()!=NULL){
+        if(nP==NULL)
             raiz=r->dameDer();
-        else if(r==nP->dameIzq())//Y eres hijo izquierdo de nP
+        else if(r==nP->dameIzq())
             nP->modificaIzq(r->dameDer());
-        else//Y eres hijo derecho de nP
+        else
             nP->modificaDer(r->dameDer());
 
         delete r;
         return true;
     }
-    else if(d==r->dameDato() && r->dameIzq()!=NULL && r->dameDer()!=NULL){//Tienes 2 hijos
+    else if(d==r->dameDato().dameNumero() && r->dameIzq()!=NULL && r->dameDer()!=NULL){
         traeMenorNodo(r,r->dameDer(),r);
         return true;
     }
-    else if(d<r->dameDato() && r->dameIzq()==NULL){//Buscas un numero inexistente menor a r
+    else if(d<r->dameDato().dameNumero() && r->dameIzq()==NULL){
         return false;
     }
-    else if(d>r->dameDato() && r->dameDer()==NULL){//Buscas un numero inexistente mayor a r
+    else if(d>r->dameDato().dameNumero() && r->dameDer()==NULL){
         return false;
     }
-    else if(d<r->dameDato() && r->dameIzq()!=NULL)//Ve a buscar d en la rama izquierda de r
+    else if(d<r->dameDato().dameNumero() && r->dameIzq()!=NULL)
         return eliminaNodo(r,r->dameIzq(),d);
-    else if(d>r->dameDato() && r->dameDer()!=NULL)//Ve a buscar d en la rama derecha de r
+    else if(d>r->dameDato().dameNumero() && r->dameDer()!=NULL)
         return eliminaNodo(r,r->dameDer(),d);
 }
-void Arbol::traeMenorNodo(NodoA* nP,NodoA* r,NodoA* aqui){
+void ArbolP::traeMenorNodo(NodoA* nP,NodoA* r,NodoA* aqui)
+{
     if(r->dameIzq()==NULL && r->dameDer()==NULL && r==nP->dameIzq()){
         aqui->modificaDato(r->dameDato());
         nP->modificaIzq(NULL);
@@ -250,4 +314,3 @@ void Arbol::traeMenorNodo(NodoA* nP,NodoA* r,NodoA* aqui){
     else
         traeMenorNodo(r,r->dameIzq(),aqui);
 }
-
